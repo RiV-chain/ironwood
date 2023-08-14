60a14e8000c763199cb0032900ee3ee4bfd6a3ec
Vadym Vikulin <vadym.vikulin@rivchain.org>
2023-08-15 00:54:50 +0300

    New Domain constructor RIVM-121

--- types/domain.go
+++ types/domain.go
@@ -15,3 +15,10 @@ type Domain struct {
 func (a Domain) Equal(comparedDomain Domain) bool {
 	return bytes.Equal(a.Name, comparedDomain.Name) && a.Key.Equal(comparedDomain.Key)
 }
+
+func NewDomain(name string, key ed25519.PublicKey) Domain {
+	return Domain{
+		Key:  key,
+		Name: append([]byte(name), make([]byte, ed25519.PublicKeySize-len([]byte(name)))...),
+	}
+}
